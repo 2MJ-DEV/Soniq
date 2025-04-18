8e028952c34066f7b0d9770a188261e5f4430431
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-04-18 14:40:06 +0100

    2

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
         input: {
-            main: resolve(__dirname, '/index.html'),
+            main: resolve(__dirname, './index.html'),
             about: resolve(__dirname, 'src/pages/about.html'),
             articles: resolve(__dirname, 'src/pages/articles.html'),
             displayarticle: resolve(__dirname, 'src/pages/displayarticle.html'),
+            editprofilinfo: resolve(__dirname, 'src/pages/edit-profil-info.html'),
             profil: resolve(__dirname, 'src/pages/profil-info.html'),
             contact: resolve(__dirname, 'src/pages/contact.html'),
             login: resolve(__dirname, 'src/pages/login.html'),
